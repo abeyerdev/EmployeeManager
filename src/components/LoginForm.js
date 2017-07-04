ac0a17aa10af5d0e35d4657abd25d8204f37d5f4
Adam Beyer <abeyerdev@gmail.com>
2017-07-04 11:02:13 -0500

    finished login functionality

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
 import { connect } from 'react-redux';
 import { emailChanged, passwordChanged, loginUser } from '../actions';
-import { Button, Card, CardSection, Input } from './common';
+import { Button, Card, CardSection, Input, Spinner } from './common';
 
 class LoginForm extends Component {
   onButtonPress() {
@@ -15,6 +16,18 @@ class LoginForm extends Component {
 
   onPasswordChange(text) {
     this.props.passwordChanged(text);
+  }
+
+  renderButton() {
+    if (this.props.loading) {
+      return <Spinner size="large" />;
+    }
+
+    return (
+      <Button onPress={this.onButtonPress.bind(this)}>
+        Login
+      </Button>
+    );          
   }
 
   render() {
@@ -31,25 +44,37 @@ class LoginForm extends Component {
         <CardSection>
           <Input 
             secureTextEntry 
-            label="password" 
+            label="Password" 
             placeholder="password" 
             onChangeText={this.onPasswordChange.bind(this)}
           />
         </CardSection>
+
+        <Text style={styles.errorTextStyle}>
+          {this.props.error}
+        </Text>
+
         <CardSection>
-          <Button onPress={this.onButtonPress.bind(this)}>Login</Button>
+          {this.renderButton()}
         </CardSection>
       </Card>   
     );
   }
 }
 
-  const mapStateToProps = (state) => {
-    return {
-      email: state.auth.email,
-      password: state.auth.password
-    };
-  };
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
+  }
+};
+
+const mapStateToProps = ({ auth }) => {
+  const { email, password, error, loading } = auth;
+  
+  return { email, password, error, loading };
+};
 
 export default connect(mapStateToProps, { 
   emailChanged, 
--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,4 +1,4 @@
-import { EMAIL_CHANGED, PASSWORD_CHANGED } from '../actions/types';
+import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER, LOGIN_USER_SUCCESS, LOGIN_USER_FAIL } from '../actions/types';
 
 const INITIAL_STATE = { 
   email: '',
@@ -13,15 +13,34 @@ export default (state = INITIAL_STATE, action) => {
 
   switch (action.type) {
     case EMAIL_CHANGED: 
-      return {
-        ...state,
-        email: action.payload // this line will overwrite existing property if it exists 
+      return { 
+        ...state, 
+        email: action.payload 
       };
     case PASSWORD_CHANGED:
-      return {
-        ...state,
-        password: action.payload
+      return { 
+        ...state, 
+        password: action.payload 
       };
+    case LOGIN_USER:
+      return { 
+        ...state, 
+        loading: true, 
+        error: '' 
+      };
+    case LOGIN_USER_SUCCESS: 
+      return {     
+        ...state,    
+        ...INITIAL_STATE,        
+        user: action.payload
+      };
+    case LOGIN_USER_FAIL:
+      return { 
+        ...state, 
+        error: 'Authentication Failed', 
+        password: '', 
+        loading: false 
+      };    
     default:
       return state;
   }
